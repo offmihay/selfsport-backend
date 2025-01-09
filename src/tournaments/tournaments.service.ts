ae1e61ccc84e48b1086e587f186b8c687ab1e877
offmihay <offmihay@gmail.com>
2025-01-09 21:22:38 +0200

    enable log for prisma client

--- src/tournaments/tournaments.service.ts
+++ src/tournaments/tournaments.service.ts
@@ -3,6 +3,13 @@ import { PrismaClient, Tournament } from '@prisma/client';
 
 @Injectable()
 export class TournamentsService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: ['query', 'info', 'warn', 'error'],
+      errorFormat: 'pretty',
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
