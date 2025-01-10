ee824a5c47139484cea0bcc9c156db14f3867679
offmihay <offmihay@gmail.com>
2025-01-10 14:29:48 +0200

    extend tournament dto with additional props

--- src/tournaments/tournaments.service.ts
+++ src/tournaments/tournaments.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient, Tournament } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class TournamentsService extends PrismaClient implements OnModuleInit {
@@ -14,7 +14,49 @@ export class TournamentsService extends PrismaClient implements OnModuleInit {
     await this.$connect();
   }
 
-  async getTournaments(): Promise<Tournament[]> {
-    return this.tournament.findMany();
+  async getTournaments() {
+    const tournaments = await this.tournament.findMany({
+      include: {
+        images: true,
+      },
+    });
+
+    const extendedTournaments = tournaments.map((tournament) => {
+      return {
+        ...tournament,
+        geoCoordinates: {
+          latitude: tournament.latitude,
+          longitude: tournament.longitude,
+        },
+        ageRestrictions: {
+          minAge: tournament.minAge,
+          maxAge: tournament.maxAge,
+        },
+        currentParticipants: {
+          count: 12,
+          participants: [
+            {
+              id: 'uuid1',
+              name: 'John Doe',
+            },
+            {
+              id: 'uuid2',
+              name: 'Jane Smith',
+            },
+          ],
+        },
+        organizer: {
+          id: 'organizerId',
+          name: 'Club XYZ',
+          contact: {
+            email: 'contact@clubxyz.com',
+            phone: '+1234567890',
+          },
+          verified: true,
+        },
+      };
+    });
+
+    return extendedTournaments;
   }
 }
